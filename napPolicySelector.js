6e68bfc8c645551e73d7763088c6ea4937b2fa02
Peter Scheffler <p.scheffler@f5.com>
2022-06-20 12:15:23 -0400

    adding debug logging

--- napPolicySelector.js
+++ napPolicySelector.js
@@ -3,13 +3,16 @@ async function handleRequest(r){
     friendlyIp = r.variables["ip_flag"] == "friendly_IP" ? true : false;
     let agent = r.variables["http_user_agent"];
     let location;
+    r.log("event is firing")
 
     switch(agent){
             case "chrome":
             location = friendlyIp ? "default" : "medium";
+            r.log("executing chrome example")
             break;
         default:
             location = friendlyIp  ? "medium" : "strict";
+            r.log("executing non-chrome example")
             break;
     }
 
