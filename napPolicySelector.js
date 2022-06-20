2598a8d1b27f2c2d5b7ef0a1a5f9f5d71871c9e4
Peter Scheffler <p.scheffler@f5.com>
2022-06-20 13:30:38 -0400

    js work...

--- napPolicySelector.js
+++ napPolicySelector.js
@@ -1,31 +1,31 @@
-async function handleRequest(r){
+async function handleRequest(r) {
     let friendlyIp;
     friendlyIp = r.variables["ip_flag"] == "friendly_IP" ? true : false;
     let agent = r.variables["http_user_agent"];
     let policy_choice;
     r.error("event is firing");
 
-    r.error("U-A is: "+agent);
+    r.error("U-A is: " + agent);
 
     r.error("friendIp is " + friendlyIp);
     r.error("IP Address: " + r.variables["remote_addr"]);
 
-    switch(true){
-            case /chrome/.test(agent):
-            policy_choice = friendlyIp ? "default" : "medium";
+    switch (true) {
+        case /chrome/.test(agent):
             r.log("executing chrome example")
+            policy_choice = friendlyIp ? "default" : "medium";
             break;
         default:
-            policy_choice = friendlyIp  ? "medium" : "strict";
             r.log("executing non-chrome example")
+            policy_choice = friendlyIp ? "medium" : "strict";
             break;
     }
 
-    r.error("Location: "+policy_choice+" where we are going.")
+    r.error("Location: " + policy_choice + " where we are going.")
 
     // let reply = await r.subrequest("/"+location+"/"+r.uri)
     // r.return(reply.status, reply.responseBody);
-    r.internalRedirect("/"+policy_choice+"/"+r.uri);
+    r.internalRedirect("/" + policy_choice + "/" + r.uri);
 }
 
 
