873c937778934fd037eafd0c1d9b97ad9d04566f
Peter Scheffler <p.scheffler@f5.com>
2022-06-21 08:47:37 -0400

    working with xff

--- napPolicySelector.js
+++ napPolicySelector.js
@@ -1,18 +1,19 @@
 async function handleRequest(r) {
-    let friendlyIp;
-    friendlyIp = r.variables["ip_flag"] == "friendly_IP" ? true : false;
     let agent = r.variables["http_user_agent"];
     let policy_choice;
     r.error("event is firing");
-
     r.error("U-A is: " + agent);
-
-    r.error("friendIp is " + friendlyIp);
-    r.error("IP Address: " + r.variables["remote_addr"]);
 
     // get the XFF value
     let xff = r.headersIn["X-Forwarded-For"];
+
+    let friendlyIp;
+    friendlyIp = r.variables["ip_flag"] == xff ? true : false;
+
     r.error("XFF: " + xff);
+    r.error("friendIp is " + friendlyIp);
+    r.error("IP Address: " + r.variables["remote_addr"]);
+
 
     if (agent.toLowerCase().indexOf("chrome") > 0) {
         r.log("executing chrome example")
